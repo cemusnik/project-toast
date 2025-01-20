5e5d65b50f140ef073c4f266281d0c31add8839c
cemusnik <nikolacemusova@gmail.com>
2025-01-20 16:27:52 +0100

    Feat: Add an editable toast with variant icon

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -2,21 +2,40 @@ import React from 'react';
 
 import Button from '../Button';
 
+import Toast from '../Toast';
+
 import styles from './ToastPlayground.module.css';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [isRendered, setIsRendered] = React.useState(false);
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState(
     VARIANT_OPTIONS[0]
   );
+
+  function handleDismiss() {
+    setIsRendered(false);
+  }
+
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {isRendered && (
+        <Toast
+          variant={variant}
+          handleDismiss={handleDismiss}
+        >
+          {message}
+        </Toast>
+      )}
+
+
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -68,7 +87,7 @@ function ToastPlayground() {
           >
             <Button
               onClick={() => {
-                window.alert(`${variant} - ${message}`);
+                setIsRendered(true);
               }}
             >
               Pop Toast!
